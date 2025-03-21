3667fef3b5e503face507c7513c0b6d6878a62a2
nguyennb28 <nguyennb.coding@gmail.com>
2025-03-21 11:13:33 +0700

    adjust header and footer

--- frontend-client/src/components/Header.jsx
+++ frontend-client/src/components/Header.jsx
@@ -43,12 +43,7 @@ export default function Header() {
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img
-              alt=""
-              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
-            />
+            <span className="font-bold">T.T.P Logistics</span>
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -127,12 +122,7 @@ export default function Header() {
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
+              <span className="font-bold">T.T.P Logistics</span>
             </a>
             <button
               type="button"
