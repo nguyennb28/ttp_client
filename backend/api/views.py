f397b0300c4333dc0f32caa8d2c4537846fa8f06
nguyennb28 <nguyennb.coding@gmail.com>
2025-04-01 10:01:27 +0700

    make filter for queryset of port, vat_infos, containers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,3 @@
-from rest_framework import viewsets
 from .models import User, Port, ContainerSize, VAT_INFO
 from .serializers import (
     UserSerializer,
@@ -7,11 +6,12 @@ from .serializers import (
     VatInfoSerializer,
 )
 
+from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .permissions import IsRoleAdmin, IsRoleUser, IsRoleAdminOrUserOrEmployee
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -45,6 +45,19 @@ class PortViewSet(viewsets.ModelViewSet):
             permission_classes = [IsRoleAdmin]
         return [permission() for permission in permission_classes]
 
+    # search or filter (api/ports?q=...)
+    # search all filter in Port
+    def get_queryset(self):
+        queryset = Port.objects.all().order_by("country")
+        param = self.request.query_params.get("q")
+        if param:
+            queryset = queryset.filter(
+                Q(country__icontains=param)
+                | Q(name__icontains=param)
+                | Q(code__icontains=param)
+            )
+        return queryset
+
 
 class ContainerSizeViewSet(viewsets.ModelViewSet):
     queryset = ContainerSize.objects.all().order_by("id")
@@ -57,6 +70,17 @@ class ContainerSizeViewSet(viewsets.ModelViewSet):
             permission_classes = [IsRoleAdmin]
         return [permission() for permission in permission_classes]
 
+    def get_queryset(self):
+        queryset = ContainerSize.objects.all().order_by("id")
+        param = self.request.query_params.get("q")
+        if param:
+            queryset = queryset.filter(
+                Q(name__icontains=param)
+                | Q(size__icontains=param)
+                | Q(abbreviation__icontains=param)
+            )
+        return queryset
+
 
 class VatInfoViewSet(viewsets.ModelViewSet):
     queryset = VAT_INFO.objects.all().order_by("id")
@@ -68,3 +92,20 @@ class VatInfoViewSet(viewsets.ModelViewSet):
         else:
             permission_classes = [IsRoleAdmin]
         return [permission() for permission in permission_classes]
+
+    def get_queryset(self):
+        queryset = VAT_INFO.objects.all().order_by("id")
+        param = self.request.query_params.get("q")
+        if param:
+            queryset = queryset.filter(
+                Q(company_name__icontains=param)
+                | Q(address__icontains=param)
+                | Q(company_tax_code__icontains=param)
+                | Q(ward_or_commune__icontains=param)
+                | Q(district__icontains=param)
+                | Q(province_or_city__icontains=param)
+                | Q(country__icontains=param)
+                | Q(einvoice_contact_name__icontains=param)
+                | Q(einvoice_contact_email__icontains=param)
+            )
+        return queryset
