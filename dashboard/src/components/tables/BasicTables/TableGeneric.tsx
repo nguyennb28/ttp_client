056848c19c261c8517683d64659e8e755068253c
nguyennb28 <nguyennb.coding@gmail.com>
2025-04-14 10:03:54 +0700

    clean unnecessary line

--- dashboard/src/components/tables/BasicTables/TableGeneric.tsx
+++ dashboard/src/components/tables/BasicTables/TableGeneric.tsx
@@ -38,8 +38,6 @@ const TableGeneric: React.FC<TableGenericProps> = ({
   const onQ = (e: React.ChangeEvent<HTMLInputElement>) => {
     handleSearch(e.currentTarget.value);
   };
-  console.log(headers)
-  console.log(header_visible)
   return (
     <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
       <div className="max-w-full overflow-x-auto">
