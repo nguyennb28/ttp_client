c46fc1d604234acc57105f7080961c39a50ee24d
nguyennb28 <nguyennb.coding@gmail.com>
2025-04-14 11:01:19 +0700

    make error text display

--- dashboard/src/pages/Forms/GenericForm.tsx
+++ dashboard/src/pages/Forms/GenericForm.tsx
@@ -21,7 +21,7 @@ const GenericForm: React.FC<GenericFormProps> = ({
     {}
   );
 
-  const inputClasses = `h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3  dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30`;
+  const inputClasses = `h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-xs shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3  dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30`;
 
   const [formData, setFormData] = useState(initialValues);
   const [searchQueries, setSearchQueries] = useState<{ [key: string]: string }>(
@@ -30,6 +30,7 @@ const GenericForm: React.FC<GenericFormProps> = ({
   const [searchResults, setSearchResults] = useState<{ [key: string]: string }>(
     {}
   );
+  const [errors, setErrors] = useState(null);
 
   const handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const newQuery = event.target.value;
@@ -71,27 +72,37 @@ const GenericForm: React.FC<GenericFormProps> = ({
     switch (field.type) {
       case "text":
         return (
-          <input
-            type={field.type}
-            id={field.name}
-            name={field.name}
-            value={formData[field.name] || ""}
-            onChange={handleChange}
-            className={inputClasses}
-            placeholder={field.placeholder || ""}
-          />
+          <>
+            <input
+              type={field.type}
+              id={field.name}
+              name={field.name}
+              value={formData[field.name] || ""}
+              onChange={handleChange}
+              className={inputClasses}
+              placeholder={field.placeholder || ""}
+            />
+            {errors && (
+              <p className="text-red-600 text-xs">{errors[field.name]}</p>
+            )}
+          </>
         );
       case "number":
         return (
-          <input
-            type={field.type}
-            id={field.name}
-            name={field.name}
-            value={formData[field.name] || ""}
-            step="0.01"
-            onChange={handleChange}
-            className={inputClasses}
-          />
+          <>
+            <input
+              type={field.type}
+              id={field.name}
+              name={field.name}
+              value={formData[field.name] || ""}
+              step="0.01"
+              onChange={handleChange}
+              className={inputClasses}
+            />
+            {errors && (
+              <p className="text-red-600 text-xs">{errors[field.name]}</p>
+            )}
+          </>
         );
       case "select":
         if (field.apiSearch !== null) {
@@ -122,68 +133,96 @@ const GenericForm: React.FC<GenericFormProps> = ({
                   )
                 )}
               </select>
+              {errors && (
+                <p className="text-red-600 text-xs">{errors[field.name]}</p>
+              )}
             </>
           );
         }
         return (
-          <select
-            name={field.name}
-            id={field.name}
-            value={formData[field.name] || ""}
-            className={inputClasses}
-            onChange={handleChange}
-          >
-            <option value="">---- Select an option ----</option>
-            {field.options &&
-              field.options.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-          </select>
+          <>
+            <select
+              name={field.name}
+              id={field.name}
+              value={formData[field.name] || ""}
+              className={inputClasses}
+              onChange={handleChange}
+            >
+              <option value="">---- Select an option ----</option>
+              {field.options &&
+                field.options.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+            </select>
+            {errors && (
+              <p className="text-red-600 text-xs">{errors[field.name]}</p>
+            )}
+          </>
         );
       case "checkbox":
         return (
-          <input
-            type={field.type}
-            id={field.name}
-            name={field.name}
-            checked={formData[field.name] || false}
-            onChange={handleChange}
-            className={inputClasses}
-          />
+          <>
+            <input
+              type={field.type}
+              id={field.name}
+              name={field.name}
+              checked={formData[field.name] || false}
+              onChange={handleChange}
+              className={inputClasses}
+            />
+            {errors && (
+              <p className="text-red-600 text-xs">{errors[field.name]}</p>
+            )}
+          </>
         );
       case "email":
         return (
-          <input
-            type={field.type}
-            id={field.name}
-            name={field.name}
-            value={formData[field.name] || ""}
-            onChange={handleChange}
-            className={inputClasses}
-          />
+          <>
+            <input
+              type={field.type}
+              id={field.name}
+              name={field.name}
+              value={formData[field.name] || ""}
+              onChange={handleChange}
+              className={inputClasses}
+            />
+            {errors && (
+              <p className="text-red-600 text-xs">{errors[field.name]}</p>
+            )}
+          </>
         );
       case "password":
         return (
-          <input
-            type={field.type}
-            id={field.name}
-            name={field.name}
-            value={formData[field.name] || ""}
-            onChange={handleChange}
-          />
+          <>
+            <input
+              type={field.type}
+              id={field.name}
+              name={field.name}
+              value={formData[field.name] || ""}
+              onChange={handleChange}
+            />
+            {errors && (
+              <p className="text-red-600 text-xs">{errors[field.name]}</p>
+            )}
+          </>
         );
       case "date":
         return (
-          <input
-            type={field.type}
-            id={field.name}
-            name={field.name}
-            value={formData[field.name] || ""}
-            onChange={handleChange}
-            className={inputClasses}
-          />
+          <>
+            <input
+              type={field.type}
+              id={field.name}
+              name={field.name}
+              value={formData[field.name] || ""}
+              onChange={handleChange}
+              className={inputClasses}
+            />
+            {errors && (
+              <p className="text-red-600 text-xs">{errors[field.name]}</p>
+            )}
+          </>
         );
       default:
         return null;
@@ -212,6 +251,9 @@ const GenericForm: React.FC<GenericFormProps> = ({
     const isValid = validationForm(formData);
     if (Object.keys(isValid).length === 0) {
       onSubmit(formData);
+      setErrors({});
+    } else {
+      setErrors(isValid);
     }
   };
 
@@ -228,7 +270,7 @@ const GenericForm: React.FC<GenericFormProps> = ({
         ))}
         <button
           type="submit"
-          className="block focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          className="block focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
         >
           Submit
         </button>
