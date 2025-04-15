30e5ace6b6534d8363e6983e4aa9cdee3dbbac88
nguyennb28 <nguyennb.coding@gmail.com>
2025-04-15 09:57:05 +0700

    complete modal for CFS Detail

--- dashboard/src/pages/CFS/DetailCFS.tsx
+++ dashboard/src/pages/CFS/DetailCFS.tsx
@@ -31,10 +31,52 @@ const DetailCFS: React.FC<DetailCFSProps> = ({
           closeModal();
           setTrigger(false);
         }}
-        className="h-screen max-w-[1000px] m-4"
+        className="h-[700px] max-h-screen max-w-[1000px] m-4"
       >
-        <div>
-          <h2>{detail.ship_name}</h2>
+        <div className="no-scrollbar relative w-full max-w-[1000px] overflow-y-auto rounded-3xl bg-white p-4 dark:bg-gray-900 lg:p-11">
+          <div className="px-2 pr-14 border-b-1">
+            <h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90 uppercase">
+              cfs
+            </h4>
+          </div>
+          <div className="border-b-2"></div>
+          <div className="w-full mt-5 px-2 grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 justify-between">
+            {detail && (
+              <>
+                <div>
+                  Ship name:{" "}
+                  {detail["ship_name"] ? detail["ship_name"] : "Unknow"}
+                </div>
+                <div>
+                  Cotainer number: {detail["mbl"] ? detail["mbl"] : "Unknow"}
+                </div>
+                <div>
+                  Agency:{" "}
+                  {detail["agency_name"] ? detail["agency_name"] : "Unknow"}
+                </div>
+                <div>
+                  Container size:{" "}
+                  {detail["size"] && detail["container_type"]
+                    ? `${detail["size"]} - ${detail["container_type"]}`
+                    : "Unknow"}
+                </div>
+                <div>
+                  Port: {detail["port_name"] ? detail["port_name"] : "Unknow"}
+                </div>
+                <div>CBM: {detail["cbm"] ? detail["cbm"] : "Unknow"}</div>
+                <div>ETA: {detail["eta"] ? detail["eta"] : "Unknow"}</div>
+                <div>
+                  Actual date:{" "}
+                  {detail["actual_date"] ? detail["actual_date"] : "Unknow"}
+                </div>
+                <div>
+                  End date:{" "}
+                  {detail["enda_date"] ? detail["end_date"] : "Unknow"}
+                </div>
+                <div>Note: {detail["note"] ? detail["note"] : "Empty"}</div>
+              </>
+            )}
+          </div>
         </div>
       </Modal>
     </>
